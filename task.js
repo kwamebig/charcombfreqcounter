1b5297a4837a497652c7d28844554530d4a1c383
Dmytro Korotaiev <kwamebig@gmail.com>
2021-04-27 00:42:36 +0300

    formatting

--- task.js
+++ task.js
@@ -1,25 +1,26 @@
-let elementsObject = {};
-
 const freq = (n, str) => {
     // remove unwanted characters and convert the rest to lowercase
-    let sss = str.toLowerCase().replace(/[^a-z0-9]/g, ''); 
+    let newStr = str.toLowerCase().replace(/[^a-z0-9]/g, ''); 
     // counting characters combinations
-    for (var i = 1; i < sss.length; i++) {
-        for (var j = 0; j < sss.length; j++) {
-            let element = sss.substring(j, j + i);
-            elementsObject[element] = (sss.split(element).length - 1);
+    let elementsObj = {};
+    for (var i = 1; i < newStr.length; i++) {
+        for (var j = 0; j < newStr.length; j++) {
+            let element = newStr.substring(j, j + i);
+            elementsObj[element] = newStr.split(element).length - 1;
         }
     } 
     // convert an object into an array
-    let asArray = Object.entries(elementsObject);
+    let elementsArr = Object.entries(elementsObj);
     // filter an array by number of characters 
-    const elementsArr = asArray.filter(([key, value]) => key.length === n);
+    const filteredArr = elementsArr.filter(([key, value]) => key.length === n);
     // sort an array from the greatest to the least and limit number of elements to 10
-    let sortedArr = elementsArr.sort((a,b) => {
+    let sortedArr = filteredArr.sort((a,b) => {
         return b[1] - a[1];
     }).slice(0, 10);
 
-    return sortedArr;
-}
+    return sortedArr; 
+};
 
-console.log(freq(3, "abracadabra"));
+// Example
+console.log(freq(3, "abracadabra")); 
+// [[ 'abr', 2 ],[ 'bra', 2 ],[ 'rac', 1 ],[ 'aca', 1 ],[ 'cad', 1 ],[ 'ada', 1 ],[ 'dab', 1 ]]
